0bed557edc8daafffce8a9e0ddb52b3666fdc36b
AnastasiaKuznetsova13 <125843109+AnastasiaKuznetsova13@users.noreply.github.com>
2023-04-24 00:33:11 -0400

    Update a _algorithm.py

--- a _algorithm.py
+++ a _algorithm.py
@@ -103,7 +103,7 @@ def astar(board, start, end, lose, loseB):
 
             # Create the f, g, and h values
             child.g = current_node.g + 1
-            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.h = abs((child.position[0] - end_node.position[0])) + abs((child.position[1] - end_node.position[1]))
             child.f = child.g + child.h
 
             # Child is already in the open list
